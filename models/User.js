e43e121ea48de50f0dbb5c94a776fe4386d629b2
Eric Lee <eric.hyunil.lee@gmail.com>
2024-04-16 16:24:49 -0700

    Created basic folder structures and basic boiler plate files

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email required"],
+      unique: true,
+      validate: {
+        validator: function(text) {
+            return /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(text);
+        },
+        message: "Please enter a valid email"
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'user',
+        },
+      ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
